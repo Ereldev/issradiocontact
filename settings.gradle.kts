9e2bc5907c177bf545af70792212655a216ae18d
Ereldev <ereldev@gmail.com>
2021-08-04 13:44:07 +0200

    Project creation from Wizard

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ISS_Radio_Contact"
+include(":android")
+include(":shared")
